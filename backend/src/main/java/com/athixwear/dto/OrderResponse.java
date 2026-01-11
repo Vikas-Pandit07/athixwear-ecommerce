a64a85e0d95bd7f46dd0e2ad619c3adb9b905bd4
Vikas Pandit <96286560+Vikas-Pandit07@users.noreply.github.com>
2026-01-11 19:11:13 +0530

    Create OrderResponse.java

---
+++ backend/src/main/java/com/athixwear/dto/OrderResponse.java
@@ -0,0 +1,65 @@
+package com.athixwear.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderResponse {
+	private Long orderId;
+	private BigDecimal totalAmount;
+	private String orderStatus;
+	private String paymentStatus;
+	private LocalDateTime orderDate;
+	private String paymentMethod;
+	private String shippingAddress;
+	private List<OrderItemResponse> items;
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public BigDecimal getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(BigDecimal totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	public List<OrderItemResponse> getItems() {
+		return items;
+	}
+	public void setItems(List<OrderItemResponse> items) {
+		this.items = items;
+	}
+	
+}
