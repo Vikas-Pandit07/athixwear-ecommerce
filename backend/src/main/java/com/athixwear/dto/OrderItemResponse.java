f2d891ae0e6ed69a793990a3f597581ecf4e0dfb
Vikas Pandit <96286560+Vikas-Pandit07@users.noreply.github.com>
2026-01-20 22:40:45 +0530

    Update OrderItemResponse.java

--- backend/src/main/java/com/athixwear/dto/OrderItemResponse.java
+++ backend/src/main/java/com/athixwear/dto/OrderItemResponse.java
@@ -3,42 +3,55 @@ package com.athixwear.dto;
 import java.math.BigDecimal;
 
 public class OrderItemResponse {
-	
-	private String productName;
-	private String productImage;
-	private Integer quantity;
-	private BigDecimal price;
-	private BigDecimal subTotal;
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public String getProductImage() {
-		return productImage;
-	}
-	public void setProductImage(String productImage) {
-		this.productImage = productImage;
-	}
-	public Integer getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-	public BigDecimal getPrice() {
-		return price;
-	}
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-	public BigDecimal getSubTotal() {
-		return subTotal;
-	}
-	public void setSubTotal(BigDecimal subTotal) {
-		this.subTotal = subTotal;
-	}
-	
-
+    
+    private Integer orderItemId;
+    private Integer productId;
+    private String productName;
+    private String productImage;
+    private Integer quantity;
+    private BigDecimal price;
+    private BigDecimal totalPrice;
+   
+    public Integer getOrderItemId() { 
+    	return orderItemId; 
+    }
+    public void setOrderItemId(Integer orderItemId) { 
+    	this.orderItemId = orderItemId; 
+    }
+    public Integer getProductId() { 
+    	return productId; 
+    }
+    public void setProductId(Integer productId) { 
+    	this.productId = productId; 
+    }
+    public String getProductName() { 
+    	return productName; 
+    }
+    public void setProductName(String productName) { 
+    	this.productName = productName; 
+    }
+    public String getProductImage() {
+    	return productImage; 
+    }
+    public void setProductImage(String productImage) { 
+    	this.productImage = productImage; 
+    }   
+    public Integer getQuantity() { 
+    	return quantity; 
+    }
+    public void setQuantity(Integer quantity) { 
+    	this.quantity = quantity; 
+    }
+    public BigDecimal getPrice() { 
+    	return price; 
+    }
+    public void setPrice(BigDecimal price) { 
+    	this.price = price; 
+    }  
+    public BigDecimal getTotalPrice() { 
+    	return totalPrice; 
+    }
+    public void setTotalPrice(BigDecimal totalPrice) { 
+    	this.totalPrice = totalPrice; 
+    }
 }
