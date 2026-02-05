3b2152f3af7767aff6c72e04b680771499ce3a91
Vikas Pandit <vikaaspanditt369@gmail.com>
2026-02-05 23:27:10 +0530

    add cartprovide in main.jsx and updating cartpage for removing external api calls and business logic

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/athixwear-ecommerce">
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>,
 );
