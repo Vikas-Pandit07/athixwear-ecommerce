92d983922405eb0a6c2f9bdf51b11487ca31ffc6
Vikas Pandit <vikaaspanditt369@gmail.com>
2026-02-20 23:09:26 +0530

    chore(main): wrap app with CheckoutProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
+import { CheckoutProvider } from "./context/CheckoutContext";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/athixwear-ecommerce">
     <AuthProvider>
       <CartProvider>
-        <App />
+        <CheckoutProvider>
+          <App />
+        </CheckoutProvider>
       </CartProvider>
     </AuthProvider>
   </BrowserRouter>,
