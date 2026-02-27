598dff687970d91767c04e44de061c0b6bfd29a3
Vikas Pandit <vikaaspanditt369@gmail.com>
2026-02-27 23:12:40 +0530

    feat: add CreateProductRequest DTO

---
+++ backend/src/main/java/com/athixwear/dto/admin/CreateProductRequest.java
@@ -0,0 +1,43 @@
+package com.athixwear.dto.admin;
+
+import java.math.BigDecimal;
+
+public class CreateProductRequest {
+	
+	private String name;
+	private String description;
+	private BigDecimal price;
+	private Integer stock;
+	private Integer categoryId;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public Integer getStock() {
+		return stock;
+	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+}
